03b7e18820afe67b80ec2f4736d801615c576939
YoLoveLife <YoLoveLife@outlook.com>
2018-08-06 18:44:40 +0800

    db import done

--- src/views/deveops/zdb/instance.vue
+++ src/views/deveops/zdb/instance.vue
@@ -59,7 +59,7 @@
 
       <el-table-column width="200px" align="center" label="连接地址">
         <template slot-scope="db">
-          <span>{{ db.row.connect_ip }}</span>
+          <span>{{ db.row._connect_ip }}</span>
         </template>
       </el-table-column>
 
@@ -88,13 +88,78 @@
           <el-button type="primary" round @click="handleImport">实例入库</el-button>
         </el-col>
         <el-col :span="11">
-          <el-button type="primary" round @click="dialogDBCreate=true">新建实例</el-button>
+          <el-button type="primary" round @click="handleCreate">新建实例</el-button>
         </el-col>
       </el-row>
     </el-dialog>
 
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogDBCreate" width="40%" top="20vh">
+      <el-form ref="createForm" :model="commit_obj" label-position="left" label-width="100px">
+        <el-form-item label="实例名称" prop="name" size="medium">
+          <el-tooltip content="请输入该实例的名称" placement="bottom" effect="light">
+            <el-input v-model="commit_obj.name"></el-input>
+          </el-tooltip>
+        </el-form-item>
+
+        <el-form-item label="所属实例组" prop="group" size="medium">
+          <el-select v-model="commit_obj.group" placeholder="请选择" filterable>
+            <el-option
+              v-for="item in instancegroups"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="所属应用组" prop="group" size="medium">
+          <el-select v-model="temp_group" placeholder="请选择" @change="init_hosts" filterable>
+            <el-option
+              v-for="item in groups"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="部署主机" prop="host" size="medium">
+          <el-select v-model="commit_obj.host" placeholder="请选择" clearable filterable>
+            <el-option
+              v-for="item in hosts"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+      <el-row>
+        <el-col :span="11">
+          <el-form-item label="实例内存" prop="detail.memory" size="medium">
+            <el-tooltip content="请输入该实例内存" placement="bottom" effect="light">
+              <el-input v-model="commit_obj.detail.memory"></el-input>
+            </el-tooltip>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="11">
+          <el-form-item label="部署端口" prop="detail.port" size="medium">
+            <el-tooltip content="请输入部署端口" placement="bottom" effect="light">
+              <el-input v-model="commit_obj.detail.port"></el-input>
+            </el-tooltip>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogDBCreate = false" :disabled="btnStatus">取消</el-button>
+        <el-button @click="createInstance" type="primary" :disabled="btnStatus">提交</el-button>
+      </div>
+
+
+    </el-dialog>
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogDBImport" width="40%" top="20vh">
-      <el-form ref="dataForm" :model="commit_obj" label-position="left" label-width="100px">
+      <el-form ref="importForm" :model="commit_obj" label-position="left" label-width="100px">
         <el-form-item label="实例名称" prop="name" size="medium">
           <el-tooltip content="请输入该实例的名称" placement="bottom" effect="light">
             <el-select v-model="formselect" @change="reset_commit" placeholder="请选择" clearable>
@@ -135,9 +200,9 @@
             </el-tooltip>
           </el-form-item>
 
-          <el-form-item label="连接地址" prop="connect_ip" size="medium">
+          <el-form-item label="连接地址" prop="_connect_ip" size="medium">
             <el-tooltip content="请输入RDS连接地址" placement="bottom" effect="light">
-              <el-input v-model="commit_obj.connect_ip"></el-input>
+              <el-input v-model="commit_obj._connect_ip"></el-input>
             </el-tooltip>
           </el-form-item>
         </div>
@@ -153,17 +218,6 @@
               </el-option>
             </el-select>
           </el-form-item>
-
-          <!-- <el-form-item label="部署主机" prop="host" size="medium">z`
-            <el-select v-model="commit_obj.host" placeholder="请选择" filterable>
-              <el-option
-                v-for="item in hosts"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
-          </el-form-item> -->
 
         <el-form-item label="部署主机" prop="host" size="medium">
           <el-select v-model="commit_obj.host" placeholder="请选择" clearable filterable>
@@ -232,7 +286,10 @@
           hosts:[],
           formselect: null,
           detailSearch: false,
-          commit_obj: {},
+          commit_obj: {
+            detail:{
+            }
+          },
           search_obj: {}
         }
       },
@@ -305,7 +362,10 @@
           this.search_obj = {}
         },
         reset_commit(){
-          this.commit_obj = {}
+          this.commit_obj = {
+              detail:{
+            }
+          }
         },
         resetCommit(value){
           console.log('ddr')
@@ -351,6 +411,9 @@
         },
         handleCreate(){
           this.dialogDBCreate = true
+          this.dialogStatus = 'create'
+          this.init_group()
+          this.hosts = []
         },
         handleUpdate(row){
           this.commit_obj = Object.assign({}, row) // copy obj
@@ -358,7 +421,7 @@
           this.dialogStatus = 'update'
           this.dialogDBVisible = true
           this.$nextTick(() => {
-            this.$refs['dataForm'].clearValidate()
+            this.$refs['importForm'].clearValidate()
           })
         },
         handleDelete(row){
@@ -388,19 +451,19 @@
           this.dialogStatus = 'update'
           this.dialogDBDetailVisible = true
           this.$nextTick(() => {
-            this.$refs['dataForm'].clearValidate()
+            this.$refs['importForm'].clearValidate()
           })
         },
         handleExpired(){
             this.$router.push({path:'/db/expired'})
         },
         createInstance(){
-          this.$refs['dataForm'].validate((valid) => {
+          this.$refs['createForm'].validate((valid) => {
             if (valid) {
               this.btnStatus=true
               create_DBInstance(this.commit_obj).then(() => {
                 this.init()
-                this.dialogDBVisible = false
+                this.dialogDBCreate = false
                 this.$message({
                   showClose: true,
                   message: '创建成功',
@@ -409,15 +472,16 @@
                 this.btnStatus=false
               }).catch((error)=>{
                 this.btnStatus=false
-                this.dialogDBVisible = false
+                this.dialogDBCreate = false
               })
             }
           })
         },
         importInstance(){
-          this.$refs['dataForm'].validate((valid) => {
+          this.$refs['importForm'].validate((valid) => {
             if (valid) {
               this.btnStatus=true
+              console.log(this.commit_obj)
               import_DBInstance(this.commit_obj).then(() => {
                 this.init()
                 this.dialogDBChoiceType = false
